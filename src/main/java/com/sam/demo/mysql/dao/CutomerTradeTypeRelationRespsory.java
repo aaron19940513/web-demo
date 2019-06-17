b90fe7a0d2b39b0c0f98980539e238bedcbea4de
gaojianchao <645358727@qq.com>
2019-06-17 23:18:13 +0800

    resolve bugs

---
+++ src/main/java/com/sam/demo/mysql/dao/CutomerTradeTypeRelationRespsory.java
@@ -0,0 +1,10 @@
+package com.sam.demo.mysql.dao;
+
+
+import com.sam.demo.mysql.entity.CustomerTradetypeRelationEntity;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CutomerTradeTypeRelationRespsory extends CrudRepository<CustomerTradetypeRelationEntity,String> {
+
+}
